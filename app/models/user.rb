af3730197996d330a47dfdf0ffe092bd616e5a60
Amed <amed157@hotmail.com>
2017-10-25 13:42:33 -0500

    add limit seassion user with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  devise :timeoutable, timeout_in: 60.minutes
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
