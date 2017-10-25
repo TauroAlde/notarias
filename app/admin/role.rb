cfdf3d2127f0b7caaa3ea551ce33976eed46c0ab
Amed <amed157@hotmail.com>
2017-10-25 13:54:14 -0500

    add crud to role model with active admin

--- app/admin/role.rb
+++ app/admin/role.rb
@@ -1,15 +1,20 @@
 ActiveAdmin.register Role do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  permit_params :name
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    actions
+  end
+
+  filter :name
+
+  form do |f|
+    f.inputs do
+      f.input :name
+    end
+    f.actions
+  end
 
 end
