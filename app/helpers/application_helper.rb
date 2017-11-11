bedf81f68a7c59b3fd659641cc3ab312c5be95a9
alexisraca <alexisraca@hotmail.com>
2017-11-11 16:05:54 -0500

    menu helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def left_sidebar_link(text, path, icon, options = {})
+    content_tag :div, class: "menu-row row" do
+      concat(
+        content_tag(:div, class: "menu-link-container") do
+          concat link_to(text, path, class: "sidebar-link")
+        end
+      )
+
+      concat(
+        content_tag(:div, class: "menu-icon-container") do
+          concat fa_icon(icon)
+        end
+      )
+    end
+  end
 end
