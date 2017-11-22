94fa9fb2d5bc206e26deacdb8115856084b63bbb
maverickrolex <roga.zero@gmail.com>
2017-11-22 00:29:38 -0500

    Add messages from users views in languages files

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
     devise_for :users
     ActiveAdmin.routes(self)
     root 'dashboards#index' 
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
     resources :users
     resources :dashboards
