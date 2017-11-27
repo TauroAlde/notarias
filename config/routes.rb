78679f3a43319b295bf6251adba8c7a6869c69b8
alexisraca <alexisraca@hotmail.com>
2017-11-27 15:38:02 -0500

    maquetar sidebars, arreglar localization

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  scope '/(:locale)', defaults: { locale: 'es' } do
+  scope "/:locale", defaults: { locale: 'es' } do
     devise_for :users
     ActiveAdmin.routes(self)
     root 'dashboards#index' 
@@ -8,4 +8,9 @@ Rails.application.routes.draw do
     resources :users
     resources :dashboards
   end
+
+  get '/*path', to: redirect("/#{I18n.default_locale}/%{path}"), constraints: lambda { |req| !req.path.starts_with? "/#{I18n.default_locale}/" }
+
+  # handles /
+  get '', to: redirect("/#{I18n.locale}")
 end
