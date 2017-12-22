bba33ef729ad6bc5c828eddf4a91e9e808084bc0
Amed <amed157@hotmail.com>
2017-12-22 09:14:15 -0500

    add destroy procedure

--- app/controllers/procedure_catalogs_controller.rb
+++ app/controllers/procedure_catalogs_controller.rb
@@ -11,4 +11,24 @@ class ProcedureCatalogsController < ApplicationController
   def new
     @procedure = Procedure.new 
   end
+
+  def create
+    @procedure = Procedure.new(procedure_params)
+    if @procedure.save
+      redirect_to procedure_catalogs_path
+    else
+      redirect_to new_procedure_catalog_path
+    end
+  end
+
+  def destroy
+    @procedure = Procedure.find(params[:id])
+    @procedure.destroy
+    redirect_to procedure_catalogs_path
+  end
+  private
+
+  def procedure_params
+    params.require(:procedure).permit(:name, :creator_user_id, :version)
+  end
 end
