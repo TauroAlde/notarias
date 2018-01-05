9d6cce3d866de0cd5a78b8e529ff540bee0be506
maverickrolex <roga.zero@gmail.com>
2018-01-05 12:39:27 -0500

    model\user_spect.rb refactor

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,18 +3,10 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   describe "validations" do
-    it "is valid with valid attributes" do
-      expect(build(:user)).to be_valid
-    end
-    it "is not valid without email" do
-      expect(build(:user, email: nil)).to_not be_valid
-    end
-    it "is not valid without password" do
-      expect(build(:user, password: nil)).to_not be_valid
-    end
-    it "is not valid without password_confirmation" do
-      expect(build(:user, password_confirmation: nil)).to be_valid
-    end
+    it {expect(build(:user)).to be_valid}
+    it {expect(build(:user, email: nil)).to_not be_valid}
+    it {expect(build(:user, password: nil)).to_not be_valid}
+    it {expect(build(:user, password_confirmation: nil)).to be_valid}
   end
 
   describe "asociations" do
