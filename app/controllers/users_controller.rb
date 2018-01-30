a05c0d35cdcc54e1792fb4828d70bd31160dbc78
MaverickRolex <roga.zero@gmail.com>
2018-01-30 01:38:55 -0500

    Add lockable property to users with devise (#33)
    * Add lockable property to users with devise, add lock and unlock buttons in users action buttons
    
    * Fixes in lockable task
    
    * fixed lockable method in model
    
    * Fix devise config details, routes key value format, eliminate the pipes on user_index view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,10 +40,30 @@ before_action :allow_without_password, only: [:update]
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-
     redirect_to users_path
   end
 
+  def lock
+    user = User.find(params[:id])
+    if !(current_user == user)
+      user.lock_access!
+      flash[:success] = t(:lock_access)
+    else
+      flash[:notice] = t(:cant_perform_this_action)
+    end
+    redirect_to users_path
+  end
+
+  def unlock
+    user = User.find(params[:id])
+    if !(current_user == user)
+      user.unlock_access!
+      flash[:success] = t(:unlock_access)
+    else
+      flash[:alert] = t(:cant_perform_this_action)
+    end
+    redirect_to users_path
+  end
 
   private
 
@@ -58,6 +78,5 @@ before_action :allow_without_password, only: [:update]
       params[:user].delete(:password_confirmation)
     end
   end
-
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,20 @@
 class User < ApplicationRecord
-  devise :timeoutable, timeout_in: 60.minutes
+  # devise :timeoutable, timeout_in: 60.minutes
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable, :lockable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :procedures, class_name: "Procedure", foreign_key: :creator_user
+
+  def lock_access!
+    self.locked_at = Time.now.utc
+    save  
+  end
+
+  def unlock_access!
+    self.locked_at = nil
+    save
+  end
 
 end
