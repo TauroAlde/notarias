7fbac39ca2d473d87eded6f27300177645b16412
maverickrolex <roga.zero@gmail.com>
2018-02-13 17:30:31 -0500

    Add model preference to active_admin

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -7,7 +7,7 @@ class Preference < ApplicationRecord
   validates :default_values, presence: true
   validates :field_type, presence: true
 
-  enum field_type: [:text, :textarea, :number, :select_field, :checkbox,
-                    :radio, :date, :email, :password]
+  enum field_type: { text: 0, textarea: 1, number: 2, select_field: 3, checkbox: 4,
+                    radio: 5, date: 6, email: 7, password: 8 }
 
 end
