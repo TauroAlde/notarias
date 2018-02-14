f3aec1951407afe9618904bd71efd46971dbb6a3
maverickrolex <roga.zero@gmail.com>
2018-02-14 16:05:32 -0500

    Made user relations RSpec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,9 @@ RSpec.describe User, type: :model do
     it { should have_many(:procedures).class_name('Procedure').with_foreign_key('creator_user') }
     it { should have_many(:user_preferences) }
     it { should have_many(:preferences).through(:user_preferences) }
-
+    it { should have_many(:permissions) }
+    it { should have_many(:user_groups) }
+    it { should have_many(:groups).through(:user_groups) }
   end
 
 end
