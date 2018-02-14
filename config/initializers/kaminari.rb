5b31822e06ba1573b6f54eb537027359978ce2fb
maverickrolex <roga.zero@gmail.com>
2018-02-14 15:50:45 -0500

    Change language of the comment to english

--- config/initializers/kaminari.rb
+++ config/initializers/kaminari.rb
@@ -1,5 +1,5 @@
-#Active Admin, tiene conflicto con la gema will_paginate, donde marca error en el
-#metodo "per" este metodo remplaza o sobre escribe el metodo para corregir el error
+#Active Admin, has a conflict with the will_paginate gem, where it marks error in the
+#method "per" this method replaces or overwrites the method to fix the error
 Kaminari.configure do |config|
   config.page_method_name = :per_page_kaminari
 end
