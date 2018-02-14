fdae2d92b2665081a1e40eb01b46f23ab16e842e
maverickrolex <roga.zero@gmail.com>
2018-02-14 16:56:41 -0500

    Add uniqueness validation for group name, made RSpec tests for group

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,6 @@ class Group < ApplicationRecord
   has_many :permissions
   has_many :users, through: :user_groups
   has_many :user_groups
+
+  validates_uniqueness_of :name
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    before do
+      @group = create(:group)
+    end
+    subject { @group }
+    it { should validate_uniqueness_of(:name) }
+    it { expect(build(:group, name: "other group name")).to be_valid }
+    it { expect(build(:group)).to_not be_valid }
+  end
+  describe "asociations" do
+    it { should have_many(:permissions) }
+    it { should have_many(:user_groups) }
+    it { should have_many(:users).through(:user_groups) }
+  end
 end
