2c044f339642d1df2927aa4eb477476932ae89e9
maverickrolex <roga.zero@gmail.com>
2018-02-14 10:40:35 -0500

    comments are added with code description

--- config/initializers/kaminari.rb
+++ config/initializers/kaminari.rb
@@ -1,3 +1,5 @@
+#Active Admin, tiene conflicto con la gema will_paginate, donde marca error en el
+#metodo "per" este metodo remplaza o sobre escribe el metodo para corregir el error
 Kaminari.configure do |config|
   config.page_method_name = :per_page_kaminari
 end
