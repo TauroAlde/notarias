13732ba2c0d15d0f388a080718f75bde15700f60
alexisraca <alexisraca@hotmail.com>
2018-02-18 01:56:18 -0500

    fix migrations order for preferences field

--- db/migrate/20180206204438_create_preferences.rb
+++ db/migrate/20180206204438_create_preferences.rb
@@ -5,7 +5,7 @@ class CreatePreferences < ActiveRecord::Migration[5.0]
       t.text :description
       t.text :default_values
       t.boolean :encrypted
-      t.integer :field_type 
+      t.integer :field
       t.timestamps
     end
   end
