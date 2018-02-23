6e72d9632785b65fedbf063d462b6ed0afb71061
Amed <amed157@hotmail.com>
2018-02-22 21:39:42 -0500

    add changes for rspec_user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,21 +3,19 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   describe "asociations" do
-    it "has_many procedure" do
-      assc = described_class.reflect_on_association(:procedures)
-      expect(assc.macro).to eq :has_many
-    end
+      it { expect have_many(:procedures) }
   end
 
-  describe "validations" do    
-    it {expect(build(:user)).to be_valid}
-    it {expect(build(:user, email: nil)).to_not be_valid}
-    it {expect(build(:user, password: nil)).to_not be_valid}
-    it {expect(build(:user, password_confirmation: nil)).to be_valid}
-    it {expect(build(:user, username: nil)).to be_valid}
+  describe "validations" do
+    it { expect(build(:user)).to be_valid }
+    it { expect(build(:user, password: nil)).to_not be_valid }
+    it { expect(build(:user, password_confirmation: nil)).to be_valid }
+    it { expect(build(:user, username: nil)).to_not be_valid }
+    it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_presence_of(:username) }
-    it { expect validate_uniqueness_of(:username) }
-    it { expect allow_value('/^[a-zA-Z0-9_\.]*$/').for(:username) }
+    it { is_expected.to validate_uniqueness_of(:username).case_insensitive }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+    it { is_expected.to_not allow_value('unemail@mail.com').for(:username) }
   end
 
   describe "before_validation" do
@@ -30,60 +28,51 @@ RSpec.describe User, type: :model do
   end
   
   describe "#set_username" do
-    context "prevalidate_username_uniqueness == true" do 
-      it "returns a new username when the user is blank" do
-        user = create(:user, username: "")
-        expect(user.username).to eq(user.login)
+    context "when prevalidate_username_uniqueness = true" do
+      it "sets a username if it's blank" do
+        user = build(:user, username: "", prevalidate_username_uniqueness: true)
+        expect { user.save }.to change { user.username }
       end
 
-      it "returns a new username when the user is nil" do
-        user = create(:user, username: nil)
-        expect(user.username).to eq(user.login)
-      end
-
-      it "returns a different username when the second created user have the same username1" do
+      it "returns a new username if the username already exists" do
         username = "username1"
         user = create(:user, username: username)
-        user2 = create(:user, username: username)
-        expect(user2.username).to_not eq(username)
-      end
-
-      it "returns a different username when the username exists in the database" do
-        username = "username1"
-        user = create(:user, username: username)
-        user2 = create(:user, username: username)
-        expect(user.username).to_not eq(user2.username)
+        user2 = build(:user, username: username, prevalidate_username_uniqueness: true)
+        user2.save
+        expect(user2.username).to_not eq(user.username)
       end
     end
 
-    context "prevalidate_username_uniqueness == false" do
-      it "returns username set by user" do
-        user = create(:user, username: "username")
-        expect(user.username).to eq(user.username)
+    context " when prevalidate_username_uniqueness = false" do
+      it "not change the username" do
+        username_string = "username"
+        user1 = create(:user, username: username_string)
+        user2 = build(:user, username: username_string)
+        expect { user2.save }.not_to change { user2.username }
       end
     end 
   end
 
-  describe "#login" do #These tests are for the devise behavior, the instance form User to evaluate, has to be new, not a record saved in the database or it will never be saved in cases that are not sent or the username or email.      
-    it "returns username when user is created" do
+  describe "#login" do #These tests are for the devise behavior, the instance form User to evaluate, has to be new, not a record saved in the database or it will never be saved in cases that are not sent or the username or email.
+    it "returns username" do
       user = create(:user)
       expect(user.login).to eq(user.username)
     end
 
-    context "these cases are for unsaved user" do
-      it "when username is nil returns email" do
+    context "when user is a new record" do
+      it "returns email if username and login attributes are nil" do
         user = build(:user, email: "email", username: nil)
         expect(user.login).to eq(user.email)
       end
 
-      it "when email is nil returns username" do
+      it "returns username if login attribute is nil" do
         user = build(:user, email: nil, username: "username")
         expect(user.login).to eq(user.username)
       end
 
-      it "when email and username are nil returns login nil" do
-        user = build(:user, email: nil, username: nil)
-        expect(user.login).to eq(nil)
+      it "returns login if it isn’t nil because of conditional precedence" do
+        user = build(:user, email: nil, username: nil, login: "login")
+        expect(user.login).to eq("login")
       end
     end
   end
