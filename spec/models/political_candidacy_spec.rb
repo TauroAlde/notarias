e819ed7c05331b7668a291506251f51ba0400b38
checoleo <sergio_arteaga@hotmail.com>
2018-03-10 10:06:05 -0600

    Model politcal candidacy (#1)
    * model Political Candidacy, migration and tests
    
    * fixed

---
+++ spec/models/political_candidacy_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe PoliticalCandidacy, type: :model do
+  #TODO: model test 
+
+  #describe "asociations" do
+  #  it { is_expected.to belong_to(:political_party) }
+  #  it { is_expected.to belong_to(:candidacy) }
+  #end
+
+end
