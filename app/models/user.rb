cc17e9642916a3b8a93487cfc5b99e7c85e3da27
maverickrolex <roga.zero@gmail.com>
2018-03-16 10:26:54 -0500

    Create model UserSegment and RSpect tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
   has_many :permissions, as: :authorizable
   has_many :groups, through: :user_groups
   has_many :user_groups
+  has_many :segments, through: :user_segments
+  has_many :user_segments
   
   validates :username, uniqueness: true
   validates :username, presence: :true, uniqueness: { case_sensitive: false }
