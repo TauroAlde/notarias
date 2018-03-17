e86b9a178580e8bf53505bcbf1cec283c02b6c02
alexisraca <alexisraca@hotmail.com>
2018-03-17 16:54:41 -0500

    fix custom checkbox

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,7 +1,7 @@
 module DashboardsHelper
   def login_checkbox_tag(form_block)
     content_tag(:label, class: "custom-control custom-checkbox") do
-      concat(form_block.check_box :remember_me, class: "custom-control-input")
+      concat(check_box_tag("user[remember_me]", form_block.object.remember_me, class: "custom-control-input"))
       concat(content_tag(:span, "", class: "custom-control-indicator"))
       concat(content_tag(:span, t(:remember_me), class: "custom-control-description"))
     end
