4dcbe8788def51a8e9802756df097dea768d1b10
Benjamin <macidion@hotmail.com>
2018-03-17 20:39:10 -0500

    modelo Candidate

--- spec/models/candidacy_spec.rb
+++ spec/models/candidacy_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Candidacy, type: :model do
 
   describe "asociations" do
     it { is_expected.to belong_to(:segment) }
-    #it { is_expected.to have_many(:candidates) }
+    it { is_expected.to have_many(:candidates) }
     it { is_expected.to have_many(:political_parties).through(:political_candidacies) }
     it { is_expected.to have_many(:political_candidacies) }
   end
