457f4be3f2b06cf2bcade63f480de734ccca1e01
TauroAlde <amed157@hotmail.com>
2018-03-20 10:48:11 -0500

    add complement to the importer

--- app/models/segment_user_import.rb
+++ app/models/segment_user_import.rb
@@ -1,4 +1,5 @@
 class SegmentUserImport < ApplicationRecord
+  mount_uploader :file, UserImporterUploader
   belongs_to :Segment
   belongs_to :uploader, class_name: "User", foreign_key: :uploader_id
 end
