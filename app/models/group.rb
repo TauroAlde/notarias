a535c345c590a8f3db21345f54624602454e49b2
luky1man <macidion@hotmail.com>
2018-03-24 14:22:42 -0500

    Update group.rb

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,10 +2,8 @@ class Group < ApplicationRecord
   has_many :permissions
   has_many :users, through: :user_groups
   has_many :user_groups, inverse_of: :group
-<<<<<<< HEAD
-=======
   belongs_to :segment
->>>>>>> c33c6f2... R Segment Group
+
 
   validates_uniqueness_of :name
 end
