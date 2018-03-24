c5ad3e6fde9cbaf9b4c34aa4a1d49d0e694fe5e8
Benjamin <macidion@hotmail.com>
2018-03-24 12:56:33 -0500

    Migracion groups

--- db/migrate/20180203002344_create_groups.rb
+++ db/migrate/20180203002344_create_groups.rb
@@ -1,8 +1,7 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
-    change_table :groups do |t|
+    create_table :groups do |t|
       t.string :name
-      t.integer :segment_id
       t.timestamps
     end
   end
