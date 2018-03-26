8878e16445f44f91830f86016b96299c89e9cae3
Alexis Rabago Carvajal <alexisraca@hotmail.com>
2018-03-26 18:39:24 -0500

    Update user_segment_spec.rb

--- spec/models/user_segment_spec.rb
+++ spec/models/user_segment_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe UserSegment, type: :model do
   describe "asociations" do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to belong_to(:segment) }
-    it { is_expected.to belong_to(:representative_user).with_foreign_key('user_id').class_name('User') }
+    it { is_expected.to belong_to(:representatives).with_foreign_key('user_id').class_name('User') }
   end
 end
