7d317976de318b2540d136657e833cbcd5bce152
maverickrolex <roga.zero@gmail.com>
2018-03-26 02:18:24 -0500

    Fix representative relations for segments and make RSpec model tests

--- app/models/user_segment.rb
+++ app/models/user_segment.rb
@@ -1,5 +1,6 @@
 class UserSegment < ApplicationRecord
   belongs_to :user
   belongs_to :segment
-  belongs_to :representative, class_name: 'User', foreign_key: :user_id, ->(o) { where('user_segments.representative = ?', true) }
+  belongs_to :representative_user, ->(o) { where('user_segments.representative = ?', true) }, 
+                          class_name: "User", foreign_key: :user_id
 end
