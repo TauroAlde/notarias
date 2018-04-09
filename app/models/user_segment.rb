a6194a671a57944d4385607c2984a53cd8a51f0b
Sergio <sergio_arteaga@hotmail.com>
2018-04-09 10:36:18 -0600

    load all user in the same level or under for his representative branch

--- app/models/user_segment.rb
+++ app/models/user_segment.rb
@@ -2,4 +2,5 @@ class UserSegment < ApplicationRecord
   belongs_to :user
   belongs_to :segment
   belongs_to :representative_user, class_name: "User", foreign_key: :user_id
+  belongs_to :represented_segment, class_name: "Segment", foreign_key: :segment_id
 end
