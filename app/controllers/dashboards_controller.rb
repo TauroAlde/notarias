2f8534f7de1c1a353004011c093162698958f0cb
maverickrolex <roga.zero@gmail.com>
2018-05-06 19:32:43 -0500

    add before action to autorize dashboard manage

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,12 @@
 class DashboardsController < ApplicationController
+  before_action :autorize_manage_dashboard
 
   def index
   end
 
+  private
+
+  def autorize_manage_dashboard
+    authorize! :manage, :dashboard
+  end
 end
