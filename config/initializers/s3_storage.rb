c01b09039e542d3ec48fcf779de08d151f1af0c6
alexisraca <alexisraca@hotmail.com>
2018-05-24 02:16:47 -0500

    fog fix

--- config/initializers/s3_storage.rb
+++ config/initializers/s3_storage.rb
@@ -1,15 +1,19 @@
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],                        # required
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],                        # required
-    region:                ENV['AWS_REGION'],                  # optional, defaults to 'us-east-1'
-    host:                  "s3.us-east-2.amazonaws.com"
-  }
-  config.fog_directory  = ENV['S3_BUCKET_NAME']                                   # required
-  config.
-  config.fog_public     = true                                                 # optional, defaults to true
-  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
-  config.storage :fog
+  if Rails.env.test? || Rails.env.development?
+    config.storage :file
+  else
+    config.fog_provider = 'fog/aws'                        # required
+    config.fog_credentials = {
+      provider:              'AWS',                        # required
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],                        # required
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],                        # required
+      region:                ENV['AWS_REGION'],                  # optional, defaults to 'us-east-1'
+      host:                  "s3.us-east-2.amazonaws.com"
+    }
+    config.fog_directory  = ENV['S3_BUCKET_NAME']                                   # required
+    config.
+    config.fog_public     = true                                                 # optional, defaults to true
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
+    config.storage :fog
+  end
 end
