b5baa4bb8c057e2b575149b61fe81e3f7acf90ce
alexisraca <alexisraca@hotmail.com>
2018-05-26 23:08:40 -0500

    fix

--- app/models/parent_political_party.rb
+++ app/models/parent_political_party.rb
@@ -1,6 +1,6 @@
 class ParentPoliticalParty < ApplicationRecord
   belongs_to :political_party
-  belongs_To :parent_political_party
+  belongs_to :parent_political_party
 
   validates :parent_political_party_id, presence: true
   validates :political_party_id, presence: true
