845b0a67d1473ccc4e4eefe245d909b468bb3902
alexisraca <alexisraca@hotmail.com>
2018-06-07 02:01:47 -0500

    fixes

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,7 @@
 module ReportsHelper
+  def percent_difference_color(segment)
+    difference = segment.percent_difference
+    return "" if difference.is_a?(String)
+    difference > 10 ? "text-warning" : ""
+  end
 end
